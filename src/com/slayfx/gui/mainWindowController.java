aa1ba5803b6baf15ccee25b1b7b531f9c62fe0e3
belijzajac <tautvydas749@gmail.com>
2018-12-11 23:04:05 +0200

    Added UML class diagram as per requirements

--- src/com/slayfx/gui/mainWindowController.java
+++ src/com/slayfx/gui/mainWindowController.java
@@ -1,24 +1,17 @@
 package com.slayfx.gui;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class mainWindowController {
     @FXML private Button singlePlayerBtn;
-    @FXML private Button onlineBtn;
 
     @FXML
-    private void onSinglePlayerBtnClicked(ActionEvent event){
+    private void onSinglePlayerBtnClicked(){
         try {
             Main.gotoLevelChoosingStage();
         } catch (Exception ex) {
             System.out.println("Caught an exception: " + ex);
         }
-    }
-
-    @FXML
-    private void onOnlineBtnClicked(ActionEvent event){
-        System.out.println("Feature not implemented");
     }
 }
